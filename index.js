a083aff85c9c240af44204a2918a22fc101599e0
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-11 23:15:26 +0600

    Project started

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dotenv = require("dotenv");
+dotenv.config();
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on: ${port}`);
+});
